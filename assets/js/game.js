444d60856abbee3c397fda3d1dd33d9f1bec1364
japankid-code <japankid.jake@gmail.com>
2021-03-06 10:16:25 -0600

    add one round to fight function, add in conditional checks on health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,33 @@
-function fight() {
+let playerName = "twinky"//window.prompt("What is your robo's name?");
+let playerHealth = 100;
+let playerAttack = 10;
+
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+var fight = function() {
     window.alert(`${playerName}, the ghift has beginned.`)
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${playerName} attacked ${enemyName}.\n${enemyName} now has ${enemyHealth} remaining.`)
+    if (enemyHealth <= 0) {
+        window.alert(`${enemyName} has died!`)
+    }
+    else {
+        console.log(`${enemyName} still has ${enemyHealth} health remaining.`)
+    }
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${enemyName} attacked ${playerName}.\n${playerName} now has ${playerHealth} remaining.`)
+    if (playerHealth <= 0) {
+        window.alert(`${playerName} has died!`)
+    }
+    else {
+        console.log(`${playerName} still has ${playerHealth} health remaining.`)
+    }
 }
 
-let playerName = window.prompt("What is your robo's name?");
-window.alert(playerName);
 fight();
-
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log(`Our robot's name is ${playerName}`);
