f9d384c8880d62c757bcdaa0f57edb57cd8ca043
japankid-code <japankid.jake@gmail.com>
2021-03-05 20:58:46 -0600

    add emoji to the title ofc

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("yo")
+function fight() {
+    window.alert(`${playerName}, the ghift has beginned.`)
+}
+
+let playerName = window.prompt("What is your robo's name?");
+window.alert(playerName);
+fight();
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log(`Our robot's name is ${playerName}`);
