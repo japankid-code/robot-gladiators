a0b60e001ea5aadfed79d82ae8aad04be37df4ae
japankid-code <japankid.jake@gmail.com>
2021-03-06 14:18:15 -0600

    multiple rounds structure implemented

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,7 +56,10 @@ var fight = function(enemyName) {
 
 for (var i = 0; i < enemyNames.length; i++) {
     // debugger;
-    let pickedEnemyName = enemyNames[i]
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert(`Welcome to Robot Gladiators! Round ${i + 1}`)
+        let pickedEnemyName = enemyNames[i]
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
 }
