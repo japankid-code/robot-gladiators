d573c0c2b73384665a0a127c16f25ba985d6eaa3
japankid-code <japankid.jake@gmail.com>
2021-03-06 11:30:33 -0600

    added for loop to loop thru array of enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,33 +3,40 @@ let playerHealth = 100;
 let playerAttack = 10;
 let playerMoney = 10;
 
-let enemyName = "Roborto";
+let enemyNames = ["Roborto", "Annabelle Android", "ROBBY TREMMMBLES"];
 let enemyHealth = 50;
 let enemyAttack = 12;
 
-var fight = function() {
+// Game states
+// "WIN" = Player robot has defeated all enemy robots
+//   * Fight all enemy robos
+//   * defeat each one
+// "LOSE" - Player health less than 0
+
+var fight = function(enemyName) {
     let promptFight = window.prompt("u fightin bro?\nFIGHT or SKIP ???")
+    //
     if (promptFight === "fight" || promptFight === "FIGHT") {
         window.alert(`${playerName}, the ghift has beginned.`)
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
         enemyHealth = enemyHealth - playerAttack;
         // Log a resulting message to the console so we know that it worked.
-        console.log(`${playerName} attacked ${enemyName}.\n${enemyName} now has ${enemyHealth} remaining.`)
+        console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} remaining.`)
         if (enemyHealth <= 0) {
             window.alert(`${enemyName} has died!`)
         }
         else {
-            console.log(`${enemyName} still has ${enemyHealth} health remaining.`)
+            // console.log(`${enemyName} still has ${enemyHealth} health remaining.`)
         }
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
         // Log a resulting message to the console so we know that it worked.
-        console.log(`${enemyName} attacked ${playerName}.\n${playerName} now has ${playerHealth} remaining.`)
+        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} remaining.`)
         if (playerHealth <= 0) {
             window.alert(`${playerName} has died!`)
         }
         else {
-            console.log(`${playerName} still has ${playerHealth} health remaining.`)
+            // console.log(`${playerName} still has ${playerHealth} health remaining.`)
         }
     } else if (promptFight === "skip" || promptFight === "SKIP") {
         let confirmSkip = window.confirm("Are you certain you would like to quit?");
@@ -44,4 +51,6 @@ var fight = function() {
     }
 }
 
-fight();
+for (i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+} 
